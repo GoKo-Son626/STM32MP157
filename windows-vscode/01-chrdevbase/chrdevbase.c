0c2f8b5d79dfb88b9743101a3752fcf6e99e0ba5
GoKo Son626 <wangshuqi626@126.com>
2024-11-05 21:58:35 +0800

    chrdevbase

---
+++ windows-vscode/01-chrdevbase/chrdevbase.c
@@ -0,0 +1,10 @@
+/*
+ * @Date: 2024-11-05
+ * @LastEditors: GoKo Son626
+ * @LastEditTime: 2024-11-05
+ * @FilePath: \01-chrdevbase\chrdevbase.c
+ * @Description: 
+ */
+ 
+ #include <linux/device.h>
+
